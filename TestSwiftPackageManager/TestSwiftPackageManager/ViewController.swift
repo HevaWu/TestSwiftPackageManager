8d4d98652c66b98f929d1e62d83b75c5aa4eda31
he.wu <he.wu@linecorp.com>
2019-10-04 18:27:31 +0900

    Add Test project & add swift package manager

---
+++ TestSwiftPackageManager/TestSwiftPackageManager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestSwiftPackageManager
+//
+//  Created by ST21235 on 2019/10/04.
+//  Copyright © 2019 he.wu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
